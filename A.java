001a15dc5f098db7513f6749eb9332b9f8037589
diego-andrade1000 <90804693+diego-andrade1000@users.noreply.github.com>
2022-09-24 23:22:18 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A{
+    int A1;
+    float A2;
+
+    public int getA1(){
+        return A1;
+    }
+
+    public float getA2(){
+        return A2;
+    }
+
+    public void setA1(int _A1){
+        this.A1 = _A1;
+    }
+
+    public void setA2(float _A2){
+        this.A2 = _A2;
+    }
+
+    public void MA1(){
+        system.print.out("MA1");
+    }
+
+    public void MA2(){
+        system.print.out("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B{
+    int B1;
+    float B2;
+
+    public int getB1(){
+        return B1;
+    }
+    
+    public float getB2(){
+        return B2;
+    }
+
+    public void setB1(int _B1){
+        this.B1 = _B1;
+    }
+
+    public void setB1(float _B2){
+        this.B2 = _B2;
+    }
+
+    public void MB1(){
+        system.print.out("MB1");
+    }
+
+    public void MB2(){
+        system.print.out("MB2");
+    }
+}
